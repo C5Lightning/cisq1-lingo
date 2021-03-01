e9352ad5c11c82aa3d745849c73bbf9bea19e66f
GianniG <xlightningclawx110402@gmail.com>
2021-03-01 14:28:43 +0100

    givehint functie

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -3,7 +3,9 @@ package nl.hu.cisq1.lingo.trainer.domain;
 import lombok.Getter;
 import lombok.Setter;
 import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
+import nl.hu.cisq1.lingo.words.domain.Word;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -13,6 +15,7 @@ import java.util.stream.IntStream;
 @Getter
 @Setter
 public class Feedback {
+	private Word word;
 	private String attempt;
 	private List<Mark> hints;
 
@@ -37,6 +40,32 @@ public class Feedback {
 
 	public boolean guessIsInvalid(){
 		return hints.stream().anyMatch(m -> m == Mark.INVALID);
+	}
+
+	public ArrayList<Mark> giveHint(){
+		String w = this.word.getValue();
+		String a = this.attempt;
+		ArrayList<Mark> marks = new ArrayList<>();
+		char[] ch = new char[a.length()];
+
+		for (int i = 0; i < a.length(); i++) {
+			ch[i] = a.charAt(i);
+		}
+		for(char i : ch){
+			if (i == w.charAt(i)){
+				marks.add(Mark.CORRECT);
+			}
+			else if(w.contains(String.valueOf(i))){
+				marks.add(Mark.PRESENT);
+			}
+			else if(!w.contains(String.valueOf(i))){
+				marks.add(Mark.ABSENT);
+			}
+		}
+		for(int i = 0; i < a.length() - this.word.getLength(); i++){
+			marks.add(Mark.INVALID);
+		}
+		return marks;
 	}
 
 	@Override
